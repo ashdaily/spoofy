53d054870e95268dc9f13147dbadde0e3c9d64f8
Ash Singh <ashtokyo31@gmail.com>
2026-07-25 02:31:57 +0100

    go setup

---
+++ cmd/spoofy/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+
+func main(){
+	fmt.Println("Hi, I am Spoofy!, ready to  send traffic always.")
+
+	os.Exit(0)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ashdaily/spoofy
+
+go 1.26.2
